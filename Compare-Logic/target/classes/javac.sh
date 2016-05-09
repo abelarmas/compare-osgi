5558c2a95e25f6a5773782bca9c7dbf2eac04cbe
Abel Armas Cervantes <abel.armas@gmail.com>
2016-05-09 14:51:22 +1000

    Compare OSGi

---
+++ Compare-Logic/target/classes/javac.sh
@@ -0,0 +1,1 @@
+/bin/sh -c cd /Users/armascer/Work/Apromore/Apromore2/ApromoreCode/Compare-Logic && /Library/Java/JavaVirtualMachines/jdk1.7.0_79.jdk/Contents/Home/bin/javac @/Users/armascer/Work/Apromore/Apromore2/ApromoreCode/Compare-Logic/target/classes/org.codehaus.plexus.compiler.javac.JavacCompiler3005746336801378032arguments -J-Xmx512m -J-Xms128m
